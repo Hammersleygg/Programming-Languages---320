6170bf6ea9d9c05f81b09a0d171566f42cf37487
Hammersleygg <89542061+Hammersleygg@users.noreply.github.com>
2023-04-24 19:38:33 -0400

    Add files via upload

--- Assignment 8/Assignment8_GavinH.go
+++ Assignment 8/Assignment8_GavinH.go
@@ -1,1 +1,78 @@
+/*
+* Gavin Hammersley
+* CSCI 320
+* Assignment 8
+* April 5, 2023
+*/
 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"sort"
+	"strconv"
+)
+
+//Checking for errors 
+func errCheck (e error){
+	if e != nil {
+		panic(e)
+	}
+}
+
+//Actor Struct
+type Actor struct {
+	lastName string
+	firstName string
+	series string
+}
+
+func String(list []Actor) {
+	for _, a := range list {
+		fmt.Printf("%s %s\t%s\n", a.firstName, a.lastName, a.series)
+	}
+	fmt.Println()
+}
+
+func main() {
+	//Run Args 
+	fileName, err := os.Open(os.Args[1])
+	errCheck(err)
+
+	number, err := strconv.Atoi(os.Args[2])
+	errCheck(err)
+
+	var actors []Actor
+
+	sc := bufio.NewScanner(fileName)
+	sc.Split(bufio.ScanLines)
+
+	for sc.Scan() {
+		line := strings.Split(sc.Text(), ",")
+		temp := Actor{
+				lastName: line[0],
+				firstName: line[1],
+				series: line[2]}
+		actors = append(actors, temp)
+	}
+
+	sort.Slice(actors, func(i, j int) bool {
+		if number == 0 {
+			return actors[i].lastName < actors[j].lastName
+		}
+
+		if number == 1 {
+			return actors[i].firstName < actors[j].firstName
+		}
+
+		if number == 2 {
+			return actors[i].series < actors[j].series
+		}
+		return false
+	})
+
+	String(actors)
+}
---
+++ Assignment 8/Assingment8_GavinHammersley.md
@@ -0,0 +1,18 @@
+<!-- Assignement 8: 
+Gavin Hammersley
+CSCI 320 -->
+
+<!--
+This GO program accepts 2 command line arguments, one being a text file and the other being an Integer value (0,1,2). 
+
+0: sorts on last name
+1: sorts on first name
+2: sorts on series
+
+everything is printed in firstName lastName: series format 
+-->
+
+<!-- Running the Assignment 8 GO code 
+        Example to compile and run type this into console:
+                go run Assignment8_GavinH.go Sample_A8.txt 1
+-->
